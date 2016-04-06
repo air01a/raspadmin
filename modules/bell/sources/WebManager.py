67d8d51353f82c85a5aa8d15edf8eda977bae7cc
Erwan <Erwan@nodomain.com>
2016-04-06 13:04:22 +0000

    Bug correction on installer with bell module

--- modules/bell/sources/WebManager.py
+++ modules/bell/sources/WebManager.py
@@ -2,9 +2,19 @@ from .. import WebStructure
 import json
 import time
 import os
+import psutil
+import ConfigParser
+from subprocess import call
+from time import sleep
+
 class WebManager(WebStructure.WebAbstract):
 	def __init__(self,webconf):
 		self.webconf=webconf
+		config = ConfigParser.ConfigParser()
+		config.readfp(open('/etc/raspadmin/bell.conf'))
+		self._port=int(config.get("NET", "port"))
+		self._start="/usr/sbin/service bell start"
+		self._stop="/usr/sbin/service bell stop"
 
 	def get_html(self,http_context):
 		template=['header.tpl','bell/bell.tpl','footer.tpl']
@@ -15,14 +25,26 @@ class WebManager(WebStructure.WebAbstract):
 		if http_context.suburl=="ACK":
 			if os.path.isfile("/tmp/BELLRESPONSE"):
 				os.remove("/tmp/BELLRESPONSE")
+		
+		if http_context.suburl=="RLD":
+			call(['/usr/sbin/service','bell','start'])	
+			sleep(0.5)
+		if http_context.suburl=="STP":
+			call(['/usr/sbin/service','bell','stop'])
+			sleep(0.5)
 
 		if os.path.isfile("/tmp/BELLRESPONSE"):
 			isFile=True
 		else:
 			isFile=False
 
+		isOpen=False
+		for conn in psutil.net_connections():
+			if conn.laddr[1]==self._port and conn.type==2:
+				isOpen=True
+
 				
-		return WebStructure.HttpContext(statuscode=200,content={'isFile':isFile,'token':sessionvars['posttoken']},template=template,mimetype='text/html')
+		return WebStructure.HttpContext(statuscode=200,content={'isOpen':isOpen,'isFile':isFile,'token':sessionvars['posttoken']},template=template,mimetype='text/html')
 		
 
         def get_module_name(self):
--- modules/bell/templates/bell/bell.tpl
+++ modules/bell/templates/bell/bell.tpl
@@ -1,4 +1,12 @@
-	
+#if (@isOpen)
+Service is Running <br />
+#else
+Service is Not Running <br />
+<a href="/bell/RLD">
+<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Start</button>
+</a>
+<br />
+#end	
 #if (@isFile)
  <button type="button" class="btn btn-default btn-lg" onClick="document.location='/bell/ACK'">
                   <span class="glyphicon glyphicon-ok-sign"></span>ACK 
